ac8d2203f107010c65a6127a3221884501163948
Rafsaan Sanvir <90986677+sanvirrafsaan@users.noreply.github.com>
2025-07-29 16:51:31 -0400

    Create 1045. Customers Who Bought All Products.sql

---
+++ Advanced Select and Joins/1045. Customers Who Bought All Products.sql
@@ -0,0 +1,8 @@
+-- link to problem: https://leetcode.com/problems/customers-who-bought-all-products/description/?envType=study-plan-v2&envId=top-sql-50
+SELECT customer_id
+FROM Customer
+GROUP BY customer_id
+HAVING COUNT(DISTINCT product_key) = (
+    SELECT COUNT(DISTINCT product.product_key)
+    FROM Product
+)

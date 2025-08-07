8fd29445dc9a6146cbcbdea34f0a47d2d8ab4297
Rafsaan Sanvir <90986677+sanvirrafsaan@users.noreply.github.com>
2025-08-07 16:24:06 -0400

    Create 1164. Product Price at a Given Date.sql

---
+++ Advanced Select and Joins/1164. Product Price at a Given Date.sql
@@ -0,0 +1,19 @@
+-- link to problem: https://leetcode.com/problems/product-price-at-a-given-date/description/?envType=study-plan-v2&envId=top-sql-50
+SELECT product_id, new_price AS price
+FROM Products
+WHERE (product_id, change_date) IN (
+    SELECT product_id, MAX(change_date)
+    FROM Products 
+    WHERE change_date <= '2019-08-16'
+    GROUP BY product_id
+)
+
+UNION 
+
+SELECT product_id, 10 AS Price
+FROM Products 
+WHERE product_id NOT IN (
+    SELECT product_id
+    FROM Products 
+    WHERE change_date <= "2019-08-16"
+)
